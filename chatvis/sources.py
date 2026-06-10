416f08575eb06419f23896bc7bfb345a58d2359b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2026-06-10 10:04:03 -0500

    Use Pydantic classes for sources

--- chatvis/sources.py
+++ chatvis/sources.py
@@ -1,5 +1,3 @@
-from string import Template
-
 from chatvis.models import GeneratedPrompts, PythonCode
 
 CODE_SOURCES: list[PythonCode] = [
@@ -8,8 +6,9 @@ CODE_SOURCES: list[PythonCode] = [
         code="""
 from paraview.simple import *
 # read the input data
-ml100vtk = LegacyVTKReader(FileNames=<path>)
-        """),
+ml100vtk = LegacyVTKReader(FileNames=<input_path>)
+        """,
+    ),
     PythonCode(
         name="code_to_slice",
         code="""
@@ -20,10 +19,11 @@ slice1.SliceType = 'Plane'
 slice1.HyperTreeGridSlicer = 'Plane'
 slice1.SliceOffsetValues = [0.0]
 slice1.PointMergeMethod = 'Uniform Binning'
-"""),
+""",
+    ),
     PythonCode(
         name="code_to_contour",
-        code= """
+        code="""
 from paraview.simple import *
 # create a new contour
 contour1 = Contour(registrationName='Contour1', Input=ml100vtk)
@@ -50,7 +50,7 @@ from paraview.simple import *
 # get color transfer function/color map for 'var0'
 var0LUT = GetColorTransferFunction('var0')
 var0LUT.RGBPoints = [min, 0.0, 0.0, 0.75, (min + max) / 2.0, 0.75, 0.75, 0.75, max, 0.75, 0.0, 0.0]
-"""
+""",
     ),
     PythonCode(
         name="code_to_opacity_transfer_function",
@@ -59,91 +59,95 @@ from paraview.simple import *
 # get opacity transfer function/opacity map for 'var0'
 var0PWF = GetOpacityTransferFunction('var0')
 var0PWF.Points = [min, 0.0, 0.5, 0.0, (min + max) / 2.0, 0.5, 0.5, 0.0, max, 1.0, 0.5, 0.0]
-"""
+""",
     ),
-
     PythonCode(
-    name="code_to_create_layout",
-    code="""
+        name="code_to_create_layout",
+        code="""
 from paraview.simple import *
 # create new layout object
 layout = CreateLayout(name='Layout')
 layout.AssignView(0, renderView)
-"""),
-
+""",
+    ),
     PythonCode(
-    name="code_to_contour1Display",
-    code="""
+        name="code_to_contour1Display",
+        code="""
 from paraview.simple import *
 # show data
 contour1Display = Show(contour1, renderView)
 contour1Display.ColorArrayName = ['POINTS', '']
 contour1Display.DiffuseColor = [1.0, 0.0, 0.0]
-"""),
-
+""",
+    ),
     PythonCode(
-    name="code_to_render_view",
-    code="""
+        name="code_to_render_view",
+        code="""
 from paraview.simple import *
 # create view
 renderView = CreateView('RenderView')
 renderView.ViewSize = [1920, 1080]
-"""),
-
+""",
+    ),
     PythonCode(
-    name="code_to_render_view_direction",
-    code="""
+        name="code_to_render_view_direction",
+        code="""
 from paraview.simple import *
 # set render view direction
 renderView.ResetActiveCameraToPositiveX()
 renderView.ResetCamera()
-"""),
-
+""",
+    ),
     PythonCode(
-    name="code_to_isometric_view",
-    code="""
+        name="code_to_isometric_view",
+        code="""
 from paraview.simple import *
 # set render view direction
 renderView.ApplyIsometricView()
 renderView.ResetCamera()
-"""),
-
+""",
+    ),
     PythonCode(
-    name="code_to_save",
-    code="""
+        name="code_to_save",
+        code="""
+from paraview.simple import *
 # Save a screenshot of the render view
 SaveScreenshot(
-    '<path>',
+    '<output_path>',
     renderView,
     ImageResolution=[1920, 1080],
     OverrideColorPalette='WhiteBackground',
 )
-"""),
-}
+""",
+    ),
+]
 
-# INPUT_SOURCES: dict[str, Template] = {
-#     "ml_dvr": Template(template="I would like to use ParaView to visualize a dataset. " \
-#     "Please generate a ParaView Python script for the following operations. " \
-#     "Read in the file named '${input_filepath}'. " \
-#     "Trace streamlines of the V data array seeded from a default point cloud. " \
-#     "Render the streamlines with tubes. Add cone glyphs to the streamlines. " \
-#     "Color the streamlines and glyphs by the Temp data array. " \
-#     "View the result in the +X direction. " \
-#     "Save a screenshot of the result in the filename '${output_screenshot}'. " \
-#     "The rendered view and saved screenshot should be 1920 x 1080 pixels."),
-# }
+GENERATED_SOURCES: list[GeneratedPrompts] = [
+    GeneratedPrompts(
+        name="ml_dvr",
+        input_prompt="""
+I would like to use ParaView to visualize a dataset.
+Please generate a ParaView Python script for the following operations.
+Read in the file named '<input_path>'.
+Trace streamlines of the V data array seeded from a default point cloud.
+Render the streamlines with tubes. Add cone glyphs to the streamlines.
+Color the streamlines and glyphs by the Temp data array.
+View the result in the +X direction.
+Save a screenshot of the result in the filename '<output_path>'.
+The rendered view and saved screenshot should be 1920 x 1080 pixels.
+""",
+        generated_prompt="""
+This script uses ParaView to visualize streamlines of the V data array from the '<input_path>' file.
+Operations include reading the file, tracing streamlines, rendering with tubes, adding cone glyphs, coloring by the Temp data array, and viewing from the +X direction.
 
-# PROMPT_SOURCES: dict[str, Template] = {
-#     "ml_dvr": Template(template="""This script uses ParaView to visualize streamlines of the V data array from the ${input_filename} file.
-#                        Operations include reading the file, tracing streamlines, rendering with tubes, adding cone glyphs, coloring by the Temp data array, and viewing from the +X direction.
-
-# Requirements:
-# - Read the file '${input_filepath}'.
-# - Trace streamlines of the V data array seeded from a default point cloud.
-# - Render the streamlines with tubes for better visibility.
-# - Add cone glyphs to the streamlines to indicate direction.
-# - Color both the streamlines and glyphs using the Temp data array.
-# - Orient the view to look from the +X direction.
-# - Save a screenshot of the view at 1920 x 1080 pixels resolution to '${output_screenshot}'.
-# """)
-# }
+Requirements:
+- Read the file '<input_path>'.
+- Trace streamlines of the V data array seeded from a default point cloud.
+- Render the streamlines with tubes for better visibility.
+- Add cone glyphs to the streamlines to indicate direction.
+- Color both the streamlines and glyphs using the Temp data array.
+- Orient the view to look from the +X direction.
+- Save a screenshot of the view at 1920 x 1080 pixels resolution to '<output_path>'.
+""",
+    )
+]
